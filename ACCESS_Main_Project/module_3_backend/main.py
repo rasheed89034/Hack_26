9e0ba218716351c441c7aee4f35b42bc310ddd99
Rasheed Ahmad <rasheedahmad890127@gmail.com>
2026-08-11 23:42:02 +0500

    Update main.py

--- ACCESS_Main_Project/module_3_backend/main.py
+++ ACCESS_Main_Project/module_3_backend/main.py
@@ -96,10 +96,7 @@ allowed_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://access-frontend-6f0m.onrender.com" # 👈 Yeh exact link daalna hai
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
